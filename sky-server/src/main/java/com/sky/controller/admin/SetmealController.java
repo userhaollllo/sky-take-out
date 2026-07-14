dfa1dd287206ce3c691ac59c3a6d9c11926a27d8
hao <2020664128@qq.com>
2026-07-14 18:01:49 +0800

    分页查询套餐代码提交

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -2,16 +2,15 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/admin/setmeal")
@@ -32,5 +31,16 @@ public class SetmealController {
         setmealService.saveWithDish(setmealDTO);
 
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> page( SetmealPageQueryDTO setmealPageQueryDTO){
+        //日志
+        log.info("套餐分页查询：{}",setmealPageQueryDTO);
+        //调用service方法
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 }
