b6d75e5d245bc8e86d87c6f6383f7518a813f1f8
hao <2020664128@qq.com>
2026-07-18 15:57:15 +0800

    删除购物车业务代码

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -41,4 +41,12 @@ public class ShoppingCartController {
           shoppingCartService.cleanShoppingCart();
           return Result.success();
     }
+
+
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
 }
