7e7cce9e4193e174493fe9284c33194925e8161c
hao <2020664128@qq.com>
2026-07-22 15:56:12 +0800

    业务代码

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -1,9 +1,7 @@
 package com.sky.controller.admin;
 
 
-import com.sky.dto.OrdersConfirmDTO;
-import com.sky.dto.OrdersDTO;
-import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
@@ -53,6 +51,36 @@ public class OrderController {
     @ApiOperation("接单")
     public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO){
         orderService.confirm(ordersConfirmDTO);
+        return Result.success();
+    }
+
+
+    @PutMapping("/rejection")
+    @ApiOperation("拒单")
+    public Result rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception{
+         orderService.rejection(ordersRejectionDTO);
+         return Result.success();
+    }
+
+
+    @PutMapping("/cancel")
+    @ApiOperation("取消订单")
+    public Result cancel(OrdersCancelDTO ordersCancelDTO) throws Exception{
+        orderService.cancel(ordersCancelDTO);
+        return Result.success();
+    }
+
+    @PutMapping("/delivery/{id}")
+    @ApiOperation("派送订单")
+    public Result delivery(@PathVariable("id") Long id) {
+        orderService.delivery(id);
+        return Result.success();
+    }
+
+    @PutMapping("/complete/{id}")
+    @ApiOperation("完成订单")
+    public Result complete(@PathVariable("id") Long id) {
+        orderService.complete(id);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,6 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersConfirmDTO;
-import com.sky.dto.OrdersPageQueryDTO;
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
@@ -66,4 +63,28 @@ public interface OrderService {
     * 接单
     * */
    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+   /**
+    * 拒单
+    * */
+   void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+
+   /**
+    * 取消订单
+    * */
+   void cancel(OrdersCancelDTO ordersCancelDTO) throws  Exception;
+
+   /**
+    * 派送订单
+    *
+    * @param id
+    */
+   void delivery(Long id);
+
+   /**
+    * 完成订单
+    *
+    * @param id
+    */
+   void complete(Long id);
 }
